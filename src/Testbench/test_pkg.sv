a463bb1eefa9b9a5c15a5f062a13f7ccacabde86
Mahitha <mahitha1727@gmail.com>
2026-08-26 09:59:20 +0530

    Add test_pkg package with UVM includes

---
+++ src/Testbench/test_pkg.sv
@@ -0,0 +1,21 @@
+package test_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+`include "trans.sv"
+`include "alu_config.sv"
+`include "drv.sv"
+`include "in_mon.sv"
+`include "seqr.sv"
+`include "in_agent.sv"
+`include "out_mon.sv"
+`include "scoreboard.sv"
+`include "out_agent.sv"
+`include "subscriber.sv"
+`include "env.sv"
+`include "seq.sv"
+`include "test.sv"
+
+endpackage
+
