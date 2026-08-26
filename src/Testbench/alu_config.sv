c70427bc886e9b83b3f680bf96e0d16800fdf3df
Mahitha <mahitha1727@gmail.com>
2026-08-26 09:46:35 +0530

    Add alu_config class for UVM testbench

---
+++ src/Testbench/alu_config.sv
@@ -0,0 +1,16 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+class alu_config extends uvm_object;
+`uvm_object_utils(alu_config)
+
+  virtual alu_if vif;
+
+  uvm_active_passive_enum input_agent_is_active;
+  uvm_active_passive_enum output_agent_is_active;
+
+  function new(string name="alu_config");
+        super.new(name);
+  endfunction
+
+endclass
